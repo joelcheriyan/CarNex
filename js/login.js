9db39a0a96b4a7f8a7bdf5793ed29e823c611abd
Aliya Hameer <adhameer@gmail.com>
2014-06-10 21:30:34 -0400

    Added an about page template.

---
+++ js/login.js
@@ -0,0 +1,8 @@
+$('#log-in').popover(
+        {
+          html: true,
+          placement: 'bottom',
+          title: 'Log In',
+          content: document.getElementById('popover-content').innerHTML,
+          container: '#popover-container'
+});
