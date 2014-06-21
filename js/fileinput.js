42e038a1beb576b18f65af16800f3642c82d45e4
Aliya Hameer <adhameer@gmail.com>
2014-06-21 12:33:43 -0400

    Added a forgotten credit link to fileinput.js.

--- js/fileinput.js
+++ js/fileinput.js
@@ -1,3 +1,10 @@
+/*
+ * Copy values from a file input to an input text field that can be better
+ * styled.
+ * Modified from:
+ * http://www.surrealcms.com/blog/whipping-file-inputs-into-shape-with-bootstrap-3
+ */
+
 $(document).on('change', '.btn-file :file', function()
 {
     var label = $(this).val().replace(/\\/g, '/').replace(/.*\//, '');
