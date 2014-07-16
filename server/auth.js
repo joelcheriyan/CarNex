efbe89ef54cf322d7e9608994f7bcda3df1278b0
Joel <joel@joel-Aspire-5742.(none)>
2014-07-16 14:05:11 -0400

    working admin login

--- server/auth.js
+++ server/auth.js
@@ -3,18 +3,14 @@ var UserSchema = require('./schemas/user');
 var passport = require('passport'),
 	LocalStrategy = require('passport-local').Strategy;
 
-passport.use(new LocalStrategy(
+passport.use(new LocalStrategy(function(username,password,done){
 
-UserSchema.find({username: req.body.username, password: req.body.password})
-		.setOptions({sort: 'startdate'})
-		.exec(
-				function(username, password, done) 
-				{
-					return done(null, {username: username});	
-				}
-			),
-
-	done(null, false)
+	if(username=='admin' && password=="lynda")
+	{
+		return done(null,{username: 'admin'}) 
+	}
+	else { return done(null, false)}
+}
 
 ));
 
